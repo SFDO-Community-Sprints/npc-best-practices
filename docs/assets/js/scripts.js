9c0a08677f274748ae71f2d9ad7cb8d87b7b748f
Thaddaeus Dahlberg <16785863+tcdahlberg@users.noreply.github.com>
2024-10-03 18:28:43 -0500

    adjusted lastmodified script to use class rather than id

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -66,23 +66,18 @@ function getContentWidth(element) {
 }
 
 function setModifiedDate() {
-    let modified = '';
-    let author = '';
     fetch("https://api.github.com/repos/" + ownerName + "/" + repoName + "/commits?path=" + pagePath)
         .then((response) => {
             return response.json();
         })
         .then((commits) => {
                 //console.log(JSON.stringify(commits[0]));
-                modified = commits[0]['commit']['committer']['date'].slice(0, 10);
-                author = commits[0]['commit']['author']['name'];
+                let modified = commits[0]['commit']['committer']['date'].slice(0, 10);
+                let author = commits[0]['commit']['author']['name'];
                 document.querySelectorAll('.last-modified').forEach(function (element) {
                     element.textContent = "Last Modified: " + modified + ' by ' + author;
                 });
             }
         );
 
-    document.querySelectorAll('.last-modified').forEach(function (element) {
-        element.textContent = "Last Modified: " + modified + ' by ' + author;
-    });
 }
