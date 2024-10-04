fc72618c4ceea3e37cb2e4269e68c241d1e79bf2
Thaddaeus Dahlberg <16785863+tcdahlberg@users.noreply.github.com>
2024-10-04 10:28:44 -0500

    fixed check for Id on page now set to a queryselector

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -66,18 +66,16 @@ function getContentWidth(element) {
 }
 
 function setModifiedDate() {
-    if (document.getElementById('last-modified')) {
-        fetch("https://api.github.com/repos/" + ownerName + "/" + repoName + "/commits?path=" + pagePath)
-            .then((response) => {
-                return response.json();
-            })
-            .then((commits) => {
-                console.log(JSON.stringify(commits[0]));
-                let modified = commits[0]['commit']['committer']['date'].slice(0, 10);
-                let author = commits[0]['commit']['author']['name'];
-                document.querySelectorAll(".last-modified").forEach(function (lastModifiedElement) {
-                    lastModifiedElement.textContent = "Last Modified: " + modified + ' by ' + author;
-                });
+    fetch("https://api.github.com/repos/" + ownerName + "/" + repoName + "/commits?path=" + pagePath)
+        .then((response) => {
+            return response.json();
+        })
+        .then((commits) => {
+            console.log(JSON.stringify(commits[0]));
+            let modified = commits[0]['commit']['committer']['date'].slice(0, 10);
+            let author = commits[0]['commit']['author']['name'];
+            document.querySelectorAll(".last-modified").forEach(function (lastModifiedElement) {
+                lastModifiedElement.textContent = "Last Modified: " + modified + ' by ' + author;
             });
-    }
+        });
 }
